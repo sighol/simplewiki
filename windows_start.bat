f608b851555f55b0c8b29bfcc3bc7d8e7f6403c3
Sigurd Andreas Holsen <Sigurd.Andreas.Holsen@km.kongsberg.com>
2017-10-03 13:52:35 +0200

    Update cargo.lock

--- windows_start.bat
+++ windows_start.bat
@@ -1,1 +1,1 @@
-.\target\release\simplewiki.exe --editor "C:\Program Files\Sublime Text 3\sublime_text.exe" --port 3000 --skip-open "C:\Dev\wiki"
+simplewiki.exe --editor "C:\Program Files\Sublime Text 3\sublime_text.exe" --port 3000 --skip-open "C:\Dev\wiki"
